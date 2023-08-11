548cb916d8aed93744c195130ec7e3e8a3890b7d
romansev <romansev@wisebits.com>
2023-08-11 19:44:47 +0300

    boilerplate

--- service2/routes/api.php
+++ service2/routes/api.php
@@ -1,19 +1,10 @@
 <?php
 
-use Illuminate\Http\Request;
+declare(strict_types=1);
+
+use App\Http\Controllers\TasksController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+Route::get('/tasks', [TasksController::class, 'indexAction']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('/task', [TasksController::class, 'createAction']);
